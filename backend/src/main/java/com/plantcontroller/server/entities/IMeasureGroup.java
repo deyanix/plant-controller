107c2434c0126893cbdaabd395196e56eed0a1c1
deyanix <deyanix@deyanix.eu>
2023-04-23 13:15:00 +0200

    Better name

--- backend/src/main/java/com/plantcontroller/server/entities/IMeasureGroup.java
+++ backend/src/main/java/com/plantcontroller/server/entities/IMeasureGroup.java
@@ -1,8 +1,11 @@
 package com.plantcontroller.server.entities;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.time.LocalDateTime;
 
 public interface IMeasureGroup {
-    LocalDateTime getGroupedDate();
+    @Value("#{target.groupedDate}")
+    LocalDateTime getDate();
     double getValue();
 }
